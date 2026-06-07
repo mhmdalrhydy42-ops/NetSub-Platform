f233c882a3afcb803c2217313e5acc58c15d30b7
Mohammed <mhmdalrhydy42@gmail.com>
2026-06-07 12:17:06 +0300

    Create main landing page layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-const testBtn = document.getElementById("testBtn");
+// Mobile Menu
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
 
-testBtn.addEventListener("click", function () {
-  alert("JavaScript is working!");
+menuBtn.addEventListener("click", function () {
+  navLinks.classList.toggle("show-menu");
+});
+
+// Close menu when clicking a link
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(function (link) {
+  link.addEventListener("click", function () {
+    navLinks.classList.remove("show-menu");
+  });
+});
+
+// Dark / Light Mode
+const themeBtn = document.getElementById("themeBtn");
+const themeIcon = themeBtn.querySelector("i");
+
+themeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("light-mode");
+
+  if (document.body.classList.contains("light-mode")) {
+    themeIcon.classList.remove("fa-moon");
+    themeIcon.classList.add("fa-sun");
+  } else {
+    themeIcon.classList.remove("fa-sun");
+    themeIcon.classList.add("fa-moon");
+  }
 });
